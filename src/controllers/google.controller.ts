1c449f0bcfb13ed19cedac91666b6f1fde44899b
Mina Demian <mina@minademian.com>
2023-12-29 01:57:31 +0200

    test: wip

--- src/controllers/google.controller.ts
+++ src/controllers/google.controller.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from 'express';
+import { google } from 'googleapis';
+import config from '../services/config';
 
-export const AllRows = (_req: Request, res: Response) => {
+export const AllRows = async (req: Request, res: Response) => {
+  const auth = req.body.auth;
+  const sheets = google.sheets({ version: 'v4', auth });
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: config.GoogleSpreadsheetId,
+    range: 'Sheet1!A2:C',
+  });
+  const rows = response.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  rows.forEach((row) => {
+    console.log(`${row[0]}, ${row[1]}, ${row[2]}`);
+  });
   res.status(200).json([]);
 };
 
