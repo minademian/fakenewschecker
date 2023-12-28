4a92a44c62121e92d1c5b655db2359c96d526245
Mina Demian <mina@minademian.com>
2023-12-28 14:10:04 +0200

    fix: server wasnt running properly

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,22 @@
 import express, { Request, Response } from 'express';
-import { json } from 'body-parser';
-import crypto, { BinaryLike } from 'crypto';
-
-import config from './services/config';
-import GraphApi from './services/graph-api';
-let profile = require('./services/profile.js');
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { routes } from './routes';
 
 const app = express();
 
-app.use(json({ verify: verifyRequestSignature }));
+const json = bodyParser.json();
 
-function verifyRequestSignature(req: Request, _res: Response, buf: Buffer) {
-  const signature = req.headers['x-hub-signature'];
+app.use(json);
 
-  if (!signature) {
-    console.warn(`Couldn't find "x-hub-signature" in headers.`);
-  } else {
-    const elements = (signature as string).split('=');
-    const signatureHash = elements[1];
-    const expectedHash = crypto
-      .createHmac('sha1', config.appSecret as BinaryLike)
-      .update(buf)
-      .digest('hex');
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
+/* app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+); */
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ alive: true });
-});
-
-app.get('/profile', (req: Request, res: Response) => {
-  let token = req.query['verify_token'];
-  let mode = req.query['mode'];
-  let url = config.webhookUrl;
-
-  if (url && url.startsWith('http://'))
-    // change back to https when going live
-    res.status(200).send('ERROR - need a proper API_URL configured.');
-
-  const Profile = new profile();
-
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    if (token === config.verifyToken) {
-      if (mode == 'webhook' || mode == 'all') {
-        Profile.setWebhook();
-        res.write(
-          `<p>&#9989; Set app ${config.appId} call to ${config.webhookUrl}</p>`
-        );
-      }
-      if (mode == 'profile' || mode == 'all') {
-        Profile.setThread();
-        res.write(
-          `<p>&#9989; Set Messenger Profile of Page ${config.pageId}</p>`
-        );
-      }
-      if (mode == 'nlp' || mode == 'all') {
-        GraphApi.callNLPConfigsAPI();
-        res.write(
-          `<p>&#9989; Enabled Built-in NLP for Page ${config.pageId}</p>`
-        );
-      }
-      if (mode == 'domains' || mode == 'all') {
-        Profile.setWhitelistedDomains();
-        res.write(
-          `<p>&#9989; Whitelisted domains: ${config.whitelistedDomains}</p>`
-        );
-      }
-      if (mode == 'private-reply') {
-        Profile.setPageFeedWebhook();
-        res.write(`<p>&#9989; Set Page Feed Webhook for Private Replies.</p>`);
-      }
-      res.status(200).end();
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  } else {
-    // Returns a '404 Not Found' if mode or token are missing
-    res.sendStatus(404);
-  }
-});
+/* app.use('/', IndexRouter); */
+app.use('/', routes);
 
 export default app;
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import app from './app';
+import config from './services/config';
+
+const port = config.port;
+
+dotenv.config();
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
