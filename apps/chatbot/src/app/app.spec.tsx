09610aeea55e43406853854fb34ded3bb721d7a2
Mina Demian <mina@minademian.com>
2024-01-11 18:22:39 +0100

    test: passing e2e test for app

--- apps/chatbot/src/app/app.spec.tsx
+++ apps/chatbot/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome chatbot/gi)).toBeTruthy();
+    expect(getByText(/News Sifter/gi)).toBeTruthy();
   });
 });
