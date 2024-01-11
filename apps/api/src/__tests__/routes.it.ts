5fe0045f5a4392f9205dff86dbf69593a4469371
Mina Demian <mina@minademian.com>
2024-01-11 19:08:35 +0100

    test: update

--- apps/api/src/__tests__/routes.it.ts
+++ apps/api/src/__tests__/routes.it.ts
@@ -1,16 +1,14 @@
 import request from 'supertest';
-import app from '../server';
+import app from '../app';
 
-describe("Main route", () => {
-  it("should return 200", async () => {
-    const response = await request(app)
-      .get("/")
+describe('Main route', () => {
+  it('should return 200', async () => {
+    const response = await request(app).get('/');
 
     expect(response.status).toBe(200);
   });
-    it("should return proof of life", async () => {
-    const response = await request(app)
-      .get("/")
+  it('should return proof of life', async () => {
+    const response = await request(app).get('/');
 
     expect(response.body).toStrictEqual({ alive: true });
   });
