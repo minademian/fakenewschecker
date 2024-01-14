07450a0b10710c75dc5e841ca2f8f85e9afc587a
Mina Demian <mina@minademian.com>
2024-01-14 02:47:35 +0100

    refactor: something more production-esque

--- apps/chatbot/src/app/app.tsx
+++ apps/chatbot/src/app/app.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import '../styles.scss';
 
-import { TextField, SubmitButton, Heading } from '@newssifter/ui';
+import { SubmitButton, TextField, Heading } from '@newssifter/ui';
 
 const StyledApp = styled.div`
   // Your style here
@@ -10,14 +10,39 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <section className="bg-slate-600 p-2.5 font-serif left-1">
-        testing
-      </section>
-      <div className="bg-indigo-500 p-2 font-mono">Hello!</div>
-      <p className="py-2">Rabbit!</p>
-      <Heading></Heading>
-      <TextField></TextField>
-      <SubmitButton></SubmitButton>
+      <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32 justify-center">
+        <div
+          className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div
+          className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <Heading></Heading>
+            <TextField></TextField>
+            <SubmitButton className="border-indigo-500 bg-indigo-500 hover:bg-indigo-600"></SubmitButton>
+          </div>
+        </div>
+      </div>
     </StyledApp>
   );
 }
